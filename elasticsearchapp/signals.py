8564590a42006f108c531a2122ebc217c6ed5b57
Adam Wattis <adamjwattis@gmail.com>
2017-09-13 10:09:23 -0400

    Register signals with Django and add index name to ES model

--- elasticsearchapp/signals.py
+++ elasticsearchapp/signals.py
@@ -2,7 +2,9 @@ from .models import BlogPost
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 # Signal to save each new blog post instance into ElasticSearch
 @receiver(post_save, sender=BlogPost)
 def index_post(sender, instance, **kwargs):
+    print(instance)
     instance.indexing()
